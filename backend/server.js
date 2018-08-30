8a4df40f1b12a110b1d273d5017d14ddf7643bed
Scott <saf6260@rit.edu>
2018-08-30 15:26:09 -0400

    Minor work done for backend and frontend connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const signin = require('./controllers/signin');
 const register = require('./controllers/register');
@@ -11,10 +12,26 @@ const db = {
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).send('this is working');
+})
 
 app.post('/siginin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', register.handleRegister(db, bcrypt))
 
 const PORT = process.env.PORT
 app.listen(3000, () => {
     console.log(`app is running on port 3000`);
 })
+
+/*
+ * / --> res = this is working
+ * /sigin --> POST = success/fail
+ * /register --> POST = success/fail
+ * /profile ---> GET = user
+ * /home --> GET = user
+ * /chat --> GET = chat
+ */
